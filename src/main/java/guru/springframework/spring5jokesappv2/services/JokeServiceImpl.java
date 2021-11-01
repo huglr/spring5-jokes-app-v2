36db8b5e5733ceb5b2b817fd3766da4bb1525607
Hugues <hugues@cocktail.cc>
2021-11-01 16:49:01 +0100

    better implementation

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -4,11 +4,16 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JokeServiceImpl implements JokeService{
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
 
     @Override
-    public String getAJoke() {
-        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public String getJoke() {
         return chuckNorrisQuotes.getRandomQuote();
     }
 }
