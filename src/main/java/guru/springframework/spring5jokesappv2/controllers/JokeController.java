ecf25fe4cc9090c813d552be7515e38e15467a98
Hugues <hugues@cocktail.cc>
2021-11-02 01:26:46 +0100

    first controller

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5jokesappv2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    //@RequestMapping(“/“, “”)
+
+
+}
